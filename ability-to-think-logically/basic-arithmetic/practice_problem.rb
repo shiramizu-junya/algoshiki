1be7aa5c1249e379b18400664dda3eaa26aba11b
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-05-10 09:33:27 +0900

    [Add] 基本的な算数・前編の練習問題のコードを追加

---
+++ ability-to-think-logically/basic-arithmetic/practice_problem.rb
@@ -0,0 +1,59 @@
+#########################
+# 練習問題１
+#########################
+apple = 12
+orange = 8
+p apple - orange
+
+#########################
+# 練習問題２
+#########################
+admission_fee = 1200
+attendance_number = 350
+p admission_fee * attendance_number
+
+#########################
+# 練習問題３
+#########################
+adult_rates = 1200
+children_fare = 900
+number_of_adult = 250
+number_of_children = 40
+p adult_rates * number_of_adult + children_fare * number_of_children
+
+#########################
+# 練習問題４
+#########################
+entrance_fee = 1100
+total_admission_fee = 121000
+p total_admission_fee / entrance_fee
+
+#########################
+# 練習問題５
+#########################
+p 3.9 * 5.1
+
+#########################
+# 練習問題６
+#########################
+p (6.4 * 5.8) / 3.2
+
+#########################
+# 練習問題７
+#########################
+p (160 - 4) / 12
+
+#########################
+# 練習問題８
+#########################
+taro_age = 4
+total_people = 2
+argv = 12
+p (argv * total_people) -taro_age
+
+#########################
+# 練習問題９
+#########################
+total_age_of_three_people = 13 * 3
+total_age_of_two_people = 11 * 2
+p total_age_of_three_people - total_age_of_two_people
