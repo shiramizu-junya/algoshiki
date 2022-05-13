d70f35c7e1d1cfc1ae3d2018bca10604b826d985
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-05-13 09:54:02 +0900

    [Add] 基本的な算数・後編の練習問題コードを追加

---
+++ ability-to-think-logically/basic-arithmetic-part2/practice_problem.rb
@@ -0,0 +1,21 @@
+#########################
+# 練習問題１
+#########################
+string_lenght = 100
+one_length = 6
+p string_lenght / one_length
+
+#########################
+# 練習問題２
+#########################
+max_seats = 6
+students = 331
+p students % max_seats == 0 ? students / max_seats : students / max_seats + 1
+p (331 / 6.0).ceil
+
+#########################
+# 練習問題３
+#########################
+hp = 679
+attack = 18
+p hp / attack == 0 ? hp / attack : hp / attack + 1
