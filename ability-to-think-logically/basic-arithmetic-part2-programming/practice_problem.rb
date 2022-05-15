03d314d86527ac42cd38e065e39a36d129cab4bb
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-05-15 10:13:31 +0900

    [Add] 基本的な算数・後編 (プログラミング)問題5,6のコードを追加

--- ability-to-think-logically/basic-arithmetic-part2-programming/practice_problem.rb
+++ ability-to-think-logically/basic-arithmetic-part2-programming/practice_problem.rb
@@ -17,3 +17,17 @@ p gets.split.map(&:to_i).inject(&:%)
 n, a = gets.split.map(&:to_f)
 p n % a == 0 ? n / a : n / a + 1
 p (n / a).ceil
+
+#########################
+# 練習問題５
+#########################
+m, k = gets.split.map(&:to_f)
+p (m / k).ceil
+
+#########################
+# 練習問題６
+#########################
+u, t, a = gets.split.map(&:to_i)
+c = ((a - u) / t.to_f).ceil
+p u + t * c
+
