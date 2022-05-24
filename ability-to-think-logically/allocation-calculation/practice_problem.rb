faaac152fdbb62b53ba358dc69ad7836773b956e
Shiramizu_Jyunya <jyunya.pg.7231@gmail.com>
2022-05-24 09:30:05 +0900

    [Add] 分配算(プログラミング)の問題4,5のコードを追加

--- ability-to-think-logically/allocation-calculation/practice_problem.rb
+++ ability-to-think-logically/allocation-calculation/practice_problem.rb
@@ -9,3 +9,22 @@ puts a % 3 == 0 ? (a / 3) * 2 : -1
 a, k = gets.split.map(&:to_i)
 puts a % (k + 1) ? a / (k + 1) * k : -1
 
+# 問題４
+a, b, k = gets.split.map(&:to_i)
+y = (b - a).divmod((k + 1))
+x = k * y[0] + a
+if y[1] == 0 && y[0] >= 0
+  puts x
+else
+  puts -1
+end
+
+# 問題５
+a, b, k = gets.split.map(&:to_i)
+y = (b + a).divmod((k + 1))
+x = k * y[0] - a
+if y[1] == 0 && y[0] >= 0 && x >= 0
+  puts x
+else
+  puts -1
+end
